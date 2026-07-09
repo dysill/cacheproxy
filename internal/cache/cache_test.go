8f3c49495ef18e1c496028cc036ea921acb1c565
dysill <dylanmsill@gmail.com>
2026-07-09 00:56:51 -0700

    add more BasicCache tests

--- internal/cache/cache_test.go
+++ internal/cache/cache_test.go
@@ -28,3 +28,33 @@ func TestBasicCache_Expiry(t *testing.T) {
 		t.Errorf("expected key to be expired, but it was still found")
 	}
 }
+
+func TestBasicCache_Delete(t *testing.T) {
+	var c Cache = NewBasicCache()
+	c.Set("testkey", []byte("testval"), time.Minute)
+
+	c.Delete("testkey")
+
+	_, ok := c.Get("testkey")
+	if ok {
+		t.Errorf("expected key to not be found, but it was")
+	}
+}
+
+func TestBasicCache_Clear(t *testing.T) {
+	var c Cache = NewBasicCache()
+	c.Set("testkey1", []byte("testval1"), time.Minute)
+	c.Set("testkey2", []byte("testval2"), time.Minute)
+
+	c.Clear()
+
+	_, ok := c.Get("testkey1")
+	if ok {
+		t.Errorf("expected key to not be found, but it was")
+	}
+
+	_, ok = c.Get("testkey2")
+	if ok {
+		t.Errorf("expected key to not be found, but it was")
+	}
+}
