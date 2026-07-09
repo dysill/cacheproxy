4d3cd0fb272cfaffca7b60d9dea2ffa5bda403cc
dysill <dylanmsill@gmail.com>
2026-07-09 01:52:03 -0700

    add proxy main using basic cache

---
+++ cmd/proxy/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/dysill/cacheproxy/internal/cache"
+	"github.com/dysill/cacheproxy/internal/proxy"
+)
+
+func main() {
+	port := flag.Int("port", 8081, "Port number for proxy")
+	upstreamURL := flag.String("upstream", "http://localhost:8080", "Base URL of the upstream server")
+	ttlSeconds := flag.Int("ttl", 60, "Default Cache TTL in seconds")
+	flag.Parse()
+	defaultTTL := time.Duration(*ttlSeconds) * time.Second
+
+	c := cache.NewBasicCache()
+	h := proxy.NewProxyHandler(c, *upstreamURL, defaultTTL)
+	mux := http.NewServeMux()
+	mux.Handle("/", h)
+
+	addr := fmt.Sprintf(":%d", *port)
+	log.Printf("proxy server listening on %s", addr)
+	log.Fatal(http.ListenAndServe(addr, mux))
+}
